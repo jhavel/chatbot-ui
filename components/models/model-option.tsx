15e29fa9afc7c483c8d0dfcac1a5dbc275744058
jhavel <jmhavel@gmail.com>
2025-06-26 09:26:38 -0400

    Tooltip bug for deployment

--- components/models/model-option.tsx
+++ components/models/model-option.tsx
@@ -33,17 +33,16 @@ export const ModelOption: FC<ModelOptionProps> = ({ model, onSelect }) => {
         </div>
       }
       side="bottom"
-      trigger={
-        <div
-          className="hover:bg-accent flex w-full cursor-pointer justify-start space-x-3 truncate rounded p-2 hover:opacity-50"
-          onClick={onSelect}
-        >
-          <div className="flex items-center space-x-2">
-            <ModelIcon provider={model.provider} width={28} height={28} />
-            <div className="text-sm font-semibold">{model.modelName}</div>
-          </div>
+    >
+      <div
+        className="hover:bg-accent flex w-full cursor-pointer justify-start space-x-3 truncate rounded p-2 hover:opacity-50"
+        onClick={onSelect}
+      >
+        <div className="flex items-center space-x-2">
+          <ModelIcon provider={model.provider} width={28} height={28} />
+          <div className="text-sm font-semibold">{model.modelName}</div>
         </div>
-      }
-    />
+      </div>
+    </WithTooltip>
   )
 }
